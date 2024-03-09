3d2479d7056463ce6e34f4ee14c523d272c9a4ee
abhishekcad001 <abhishekcad001@gmail.com>
2024-03-09 12:13:44 +0530

    project set up

---
+++ src/config/constant.js
@@ -0,0 +1,8 @@
+export const BASE_URL = '/dashboard';
+export const BASE_TITLE = ' | Datta Able Premium React Hooks + Redux Admin Template';
+
+export const CONFIG = {
+  layout: 'vertical',
+  collapseMenu: false,
+  layoutType: 'menu-dark'
+};
